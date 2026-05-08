3f8e2be6353cbb5291bb0ec9bf1b9702ad51450d
rakshitahoolihalli-collab <rakshitahoolihalli@gmail.com>
2026-05-08 15:21:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,5 +43,8 @@ def main():
             except Exception as e:
                 st.error(f"Error: {e}")
 
-if _name_ == "_main_":
+def main():
+    print("App started")
+
+if __name__ == "__main__":
     main()
